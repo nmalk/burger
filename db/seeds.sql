105993206790f04b837eb2e8e3dfa2ceef050caf
Natalia Malcova <nataliamalkova@yahoo.com>
2019-09-23 16:56:36 -0400

    created files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Classic burger with cheese", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chili burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie burger", true);
---
+++ server.js
@@ -0,0 +1,28 @@
+var exphbs = require("express-handlebars");
+var express = require("express");
+
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
